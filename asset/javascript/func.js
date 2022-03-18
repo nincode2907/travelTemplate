3f3cc8deee895b0044538053737ed10bae6376b6
NNewbieDev <phuonghuy.4444@gmail.com>
2022-03-18 14:46:53 +0700

    Finished

--- asset/javascript/func.js
+++ asset/javascript/func.js
@@ -7,24 +7,20 @@ setInterval(function () {
 }, 1000)
 
 // SLIDE
-var slides = document.getElementsByClassName("slide-img");
-var slideIndex = 0;
-showSlides();
 
-function showSlides() {
-    var i;
-    //   var dots = document.getElementsByClassName("dot");
-    for (i = 0; i < slides.length; i++) {
-        slides[i].style.display = "none";
+var slides = document.getElementsByClassName('slide-img')
+
+var slideIndex = 0
+showSlide()
+function showSlide() {
+    for (var i = 0; i< slides.length; i++){
+        slides[i].style.display = "none"
     }
-    slides[]
-    slideIndex++;
+    if(slideIndex >= slides.length){
+        slideIndex = 0 
+    }
+    slideIndex++
+    slides[slideIndex-1].style.display = "block"
+    setTimeout(showSlide, 10000)
 
-    if (slideIndex > slides.length) { slideIndex = 1 }
-    //   for (i = 0; i < dots.length; i++) {
-    //     dots[i].className = dots[i].className.replace(" active", "");
-    //   }
-
-    //   dots[slideIndex-1].className += " active";
-    setTimeout(showSlides, 2000); // Change image every 2 seconds
 }
