5e4980c6d7a668b64886c037a0d7a94f81d61f11
NNewbieDev <phuonghuy.4444@gmail.com>
2022-03-18 13:23:13 +0700

    Con phan Js

--- asset/javascript/func.js
+++ asset/javascript/func.js
@@ -1,8 +1,30 @@
 // Format Date
-
-setInterval(function(){
-    var d =new Date()
+setInterval(function () {
+    var d = new Date()
     document.querySelector('.hour .time-number').innerHTML = d.getHours()
     document.querySelector('.minute .time-number').innerHTML = d.getMinutes()
     document.querySelector('.second .time-number').innerHTML = d.getSeconds()
-},1000)
+}, 1000)
+
+// SLIDE
+var slides = document.getElementsByClassName("slide-img");
+var slideIndex = 0;
+showSlides();
+
+function showSlides() {
+    var i;
+    //   var dots = document.getElementsByClassName("dot");
+    for (i = 0; i < slides.length; i++) {
+        slides[i].style.display = "none";
+    }
+    slides[]
+    slideIndex++;
+
+    if (slideIndex > slides.length) { slideIndex = 1 }
+    //   for (i = 0; i < dots.length; i++) {
+    //     dots[i].className = dots[i].className.replace(" active", "");
+    //   }
+
+    //   dots[slideIndex-1].className += " active";
+    setTimeout(showSlides, 2000); // Change image every 2 seconds
+}
